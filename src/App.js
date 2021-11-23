d65996e59e1bb7c6e9ffef588a5f4b0f56632c8a
1000099627 <adityasrivastav76@gmail.com>
2021-11-23 23:02:40 +0530

    done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Row, Container, Col } from "react-bootstrap";
 import Im1 from "../src/images/aditya.jpg"
 import Im2 from "../src/images/soham.jpeg"
+import Im3 from "../src/images/ashish.jpg"
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
           <p>
             <img src={Im1} width="200px"  height="200px"/>
             <img src={Im2} width="200px"  height="200px"/>
+            <img src={Im3} width="200px"  height="200px"/>
 
 
             
