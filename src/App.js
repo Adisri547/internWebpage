ec49b681e407aba93fed08205df95f3610979fc5
1000099627 <adityasrivastav76@gmail.com>
2021-11-24 23:54:10 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { Row, Container, Col } from "react-bootstrap";
-import Im1 from "../src/images/aditya.jpg"
-import Im2 from "../src/images/soham.jpeg"
-import Im3 from "../src/images/ashish.jpg"
+import Adi from "../src/images/aditya.jpg";
+import Soh from "../src/images/soham.jpeg";
+import Ash from "../src/images/ashish.jpg";
+import Sid from "../src/images/sid.jpg";
 function App() {
   return (
     <div className="App">
       <Container>
         <Row>
-          <p>
-            <img src={Im1} width="200px"  height="200px"/>
-            <img src={Im2} width="200px"  height="200px"/>
-            <img src={Im3} width="250px"  height="200px"/>
+          <img
+            src={Adi}
+            width="200px"
+            height="200px"
+           
+          />
 
-
-            
-          </p>
+          <img
+            src={Soh}
+            width="200px"
+            height="200px"
+           
+          />
+           <img src={Ash} width="200px" height="200px" />
+        </Row>
+        <Row>
+         
+          <img src={Sid} width="200px" height="200px" />
         </Row>
       </Container>
     </div>
