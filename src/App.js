c5149bfe5a69b2eac70749a19133c2248a7b482e
1000099627 <adityasrivastav76@gmail.com>
2021-11-26 12:24:25 +0530

    added

--- src/App.js
+++ src/App.js
@@ -5,13 +5,9 @@ import Soh from "../src/images/soham.jpeg";
 import Ash from "../src/images/ashish.jpg";
 import Sid from "../src/images/sid.jpg";
 import Shrey from "../src/images/SHREYANSH.jpeg";
-<<<<<<< HEAD
-=======
-import Git from "../src/Social Icons/Social Icons/Github.svg"
-import Link from "../src/Social Icons/Social Icons/Linkedin.svg"
-import Insta from "../src/Social Icons/Social Icons/Instagram.svg"
-
->>>>>>> af0c319d9134ebcc759f7710f1ae14702a6e90f7
+import Git from "../src/Social Icons/Social Icons/Github.svg";
+import Link from "../src/Social Icons/Social Icons/Linkedin.svg";
+import Insta from "../src/Social Icons/Social Icons/Instagram.svg";
 function App() {
   return (
     <div className="App">
@@ -22,16 +18,17 @@ function App() {
               <img class="image__img" src={Adi} />
               <div class="image__overlay image__overlay--primary">
                 <div class="image__title">Aditya Srivastav</div>
-
-<<<<<<< HEAD
-=======
-                <div class="icon"> 
-                <img src={Git} />
-                <img src={Link} />
-                <img src={Insta} />
-
+                <div class="icon">
+                  <a href="https://github.com/Adisri547">
+                    <img src={Git} height="30px" width="30px"/>
+                  </a>
+                  <a href="https://www.linkedin.com/in/aditya-srivastav-308751157/">
+                    <img src={Link} height="30px" width="30px"/>
+                  </a>
+                  <a href="https://www.instagram.com/adi.sri547/">
+                    <img src={Insta} height="30px" width="30px"/>
+                  </a>
                 </div>
->>>>>>> af0c319d9134ebcc759f7710f1ae14702a6e90f7
               </div>
             </div>
           </Col>
@@ -40,9 +37,9 @@ function App() {
               <img class="image__img" src={Soh} />
               <div class="image__overlay image__overlay--primary">
                 <div class="image__title">Soham</div>
-                <img src={Git} />
-                <img src={Link} />
-                <img src={Insta} />
+                <img src={Git} height="20px" width="20px"/>
+                <img src={Link} height="20px" width="20px" />
+                <img src={Insta} height="20px" width="20px" />
               </div>
             </div>
           </Col>
@@ -50,9 +47,9 @@ function App() {
             <img class="image__img" src={Ash} />
             <div class="image__overlay image__overlay--primary">
               <div class="image__title">Ashish Mishra</div>
-              <img src={Git} />
-                <img src={Link} />
-                <img src={Insta} />
+              <img src={Git} height="20px" width="20px"/>
+              <img src={Link} height="20px" width="20px"/>
+              <img src={Insta} height="20px" width="20px"/>
             </div>
           </div>
         </Row>
@@ -61,18 +58,18 @@ function App() {
             <img class="image__img" src={Sid} />
             <div class="image__overlay image__overlay--primary">
               <div class="image__title">Siddhant Banerjee</div>
-              <img src={Git} />
-                <img src={Link} />
-                <img src={Insta} />
+              <img src={Git} height="20px" width="20px"/>
+              <img src={Link} height="20px" width="20px"/>
+              <img src={Insta} height="20px" width="20px"/>
             </div>
           </div>
           <div class="image">
             <img class="image__img" src={Shrey} />
             <div class="image__overlay image__overlay--primary">
               <div class="image__title">Shreyansh Rai</div>
-              <img src={Git} />
-                <img src={Link} />
-                <img src={Insta} />
+              <img src={Git} height="20px" width="20px"/>
+              <img src={Link} height="20px" width="20px"/>
+              <img src={Insta} height="20px" width="20px"/>
             </div>
           </div>
         </Row>
